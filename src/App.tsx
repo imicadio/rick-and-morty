4473a2e0e01f35ac5ca55abf2774cd553b49c6e6
Michal Jeszko <michal.jeszko@sobre.pl>
2022-12-08 17:55:13 +0100

    feat: pagination

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import { ThemeProvider } from "@mui/material/styles";
 import { theme } from "./theme/theme";
-import { Box, Container, Typography, SelectChangeEvent } from "@mui/material";
+import {
+  Box,
+  Container,
+  Typography,
+  SelectChangeEvent,
+  Pagination,
+  Stack,
+} from "@mui/material";
 import React, { useEffect, useState } from "react";
 import "./App.scss";
 import Filters from "./components/filters/filters";
@@ -15,16 +22,18 @@ import {
 } from "./model.d";
 import { API_URL } from "./shared/link";
 import TableCharacters from "./components/table/table-characters";
-import CircularProgress from '@mui/material/CircularProgress';
+import CircularProgress from "@mui/material/CircularProgress";
 
 function App() {
-  const [data, setData] = useState<Record<string, any>>();
+  const [data, setData] = useState<ITableHeader[]>([]);
+  const [currentPageRequest, setCurrentPageRequest] = useState<number>(1);
   const [currentPage, setCurrentPage] = useState<number>(1);
   const [isLoading, setIsLoading] = useState<boolean>(false);
   const [rows, setRows] = useState<ITableHeader[]>([]);
   const [selected, setSelected] = useState<string[]>([]);
   const [searchWords, setSearchWords] = useState<string>("");
   const [selectSpecies, setSelectSpecies] = useState<string>("");
+  const [totalPages, setTotalPages] = useState<number>();
 
   const fetchCharacters = (
     page: number,
@@ -51,6 +60,32 @@ function App() {
     setSelectSpecies(event.target.value as string);
   };
 
+  const fnSetCurrentPageRequest = (
+    page: number,
+    diviterNumber: number
+  ): number => {
+    const actualPage = Math.ceil(page / diviterNumber);
+    return actualPage;
+  };
+
+  const handlePage = (page: number): void => {
+    setCurrentPage(page);
+
+    const dividerItems = 20 / 5;
+    const pageFrom = currentPageRequest * dividerItems - dividerItems + 1;
+    const pageTo = currentPageRequest * dividerItems;
+
+    if (pageFrom <= page && pageTo >= page) {
+      return;
+    } else if (pageTo < page) {
+      const newPage = fnSetCurrentPageRequest(page, dividerItems);
+      setCurrentPageRequest(newPage);
+    } else {
+      const newPage = fnSetCurrentPageRequest(page, dividerItems);
+      setCurrentPageRequest(newPage);
+    }
+  };
+
   const createData = (
     name: string,
     species: string,
@@ -71,8 +106,9 @@ function App() {
 
   useEffect(() => {
     setIsLoading(() => true);
-    fetchCharacters(currentPage, searchWords, selectSpecies)
+    fetchCharacters(currentPageRequest, searchWords, selectSpecies)
       .then((response) => {
+        console.log(response);
         const returnRows: ITableHeader[] = [];
         response.results.map((item: ICharacter) => {
           return returnRows.push(
@@ -87,14 +123,22 @@ function App() {
           );
         });
         setRows(returnRows);
+        setTotalPages(Math.ceil(response.info.count / 5));
       })
       .catch((error) => console.log(error));
     setIsLoading(() => false);
-  }, [currentPage, searchWords, selectSpecies]);
+  }, [currentPageRequest, searchWords, selectSpecies]);
 
   useEffect(() => {
-    // console.log(rows);
-  }, [searchWords]);
+    const dividerItems = 20 / 5;
+    const pageFrom = currentPageRequest * dividerItems - dividerItems + 1;
+    // const pageTo = currentPageRequest * dividerItems;
+    const currentPageData = currentPage - pageFrom + 1;
+    const indexOfLastProduct = currentPageData * 5;
+    const indexOfFirstProduct = indexOfLastProduct - 5;
+    const returnProducts = rows.slice(indexOfFirstProduct, indexOfLastProduct);
+    setData(returnProducts);
+  }, [rows, currentPage]);
 
   if (isLoading)
     return (
@@ -105,7 +149,12 @@ function App() {
 
   return (
     <ThemeProvider theme={theme}>
-      <Container maxWidth="lg">
+      <Container maxWidth="lg" sx={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'center'
+      }}>
         <Typography variant="h5" component="h2" fontWeight="700">
           Characters
         </Typography>
@@ -117,8 +166,20 @@ function App() {
         />
 
         <Box sx={{ width: "100%" }}>
-          <TableCharacters rows={rows} selected={selected} />
+          <TableCharacters rows={data} selected={selected} />
         </Box>
+        <Stack spacing={2} my={3}>
+          <Pagination
+            count={totalPages}
+            variant="outlined"
+            shape="rounded"
+            sx={{
+              display: "flex",
+              justifyContent: "flex-end",
+            }}
+            onChange={(e, page) => handlePage(page)}
+          />
+        </Stack>
       </Container>
     </ThemeProvider>
   );
